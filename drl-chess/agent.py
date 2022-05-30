1aa39942687b9f66322b7d61e94770573556b5dc
Paul Adamowicz <p.adamowicz18@ejm.org>
2022-05-30 18:22:26 +0200

    Day 1 modifs: Class Game, Agent & Engine

--- drl-chess/agent.py
+++ drl-chess/agent.py
@@ -2,10 +2,26 @@
 Agent module.
 """
 
+import random
+import numpy as np
+import chess.engine
+
 class Agent():
 
     def __init__(self):
         pass
 
-    def move():
-        pass
+    def move(self, observation):
+        return random.choice(np.flatnonzero(observation['action_mask']))
+
+
+
+class Engine(Agent):
+
+    def __init__(self):
+        # super().__init__(self)
+        # TODO Get current directory
+        self.transport, self.engine = chess.engine.popen_uci("dir + ../stockfish")
+
+    def engine_move(self, board):
+        return self.engine.play(board, chess.engine.Limit(time=0.1))
