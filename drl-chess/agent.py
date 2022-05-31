9b49e589a0767cac589d06115348403b5d97d90d
Paul Adamowicz <p.adamowicz18@ejm.org>
2022-05-31 10:19:17 +0200

    Mardi morning modifs

--- drl-chess/agent.py
+++ drl-chess/agent.py
@@ -5,6 +5,7 @@ Agent module.
 import random
 import numpy as np
 import chess.engine
+import os
 
 class Agent():
 
@@ -20,8 +21,15 @@ class Engine(Agent):
 
     def __init__(self):
         # super().__init__(self)
-        # TODO Get current directory
-        self.transport, self.engine = chess.engine.popen_uci("dir + ../stockfish")
+
+        # Method for StockFish in .direnv:
+        cwd = os.getcwd()
+        # print(cwd)
+        # self.transport, self.engine = chess.engine.popen_uci(f"{cwd}/../.direnv/stockfish")
+        self.engine = chess.engine.SimpleEngine.popen_uci(f"{cwd}/../.direnv/stockfish")
+        # CHANGE DIRECTORY PATH (use __file__ ?)
 
     def engine_move(self, board):
-        return self.engine.play(board, chess.engine.Limit(time=0.1))
+        return self.engine.play(board, chess.engine.Limit(time=0.1)) #Hard Code Time with config.py
+
+    # Method to convert "E4" to Action Mask
